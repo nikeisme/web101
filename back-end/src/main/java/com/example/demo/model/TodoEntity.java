3e1f5daa3a2baa4720a235798b2d4abd3fdc2050
nikeisme <nikeisme827@gmail.com>
2022-09-02 18:36:42 +0900

    git 계정 오류 해결을 위한 커밋

--- back-end/src/main/java/com/example/demo/model/TodoEntity.java
+++ back-end/src/main/java/com/example/demo/model/TodoEntity.java
@@ -11,7 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Builder // 빌더패턴을 사용해 오브젝트 생성
+@Builder // 빌더 패턴을 사용해 오브젝트 생성
 @NoArgsConstructor // 매개변수가 없는 생성자 구현
 @AllArgsConstructor// 클래스의 모든 멤버 변수를 매개변수로 받는 생성자를 구현
 @Data // 클래스 멤버 변수의 Getter/Setter 메서드를 구현
