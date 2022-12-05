5346a3eaa7cab105c3890c7ba3d97e54423f5376
nikeisme <nikeisme827@gmail.com>
2022-12-05 13:52:34 +0900

    Add : Props와State 이용하여 매개변수들을 생성자 통해 넘김

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -3,12 +3,24 @@ import Todo from './Todo';
 import './App.css';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    // (1) item -> items 배열로
+    this.state = {
+      items : [
+        {id:"0", title: "Hello World 1", done:true},
+        {id:"1", title: "Hello World 2", done:false},
+      ]
+    };
+  }
   render() {
-    return (
-      <div className='App'>
-        <Todo />
-      </div>
-    );
+   // (2) 자바스크립트가 제공하는 map 함수를 이용한 배열을 반복해 <Todo... /> 컴포넌트 생성
+   var todoItems = this.state.items.map((item,idx) =>(
+    <Todo item= {item} key={item.id} />
+   ));
+
+   //(3) 생성된 컴포넌트 리턴
+   return <div className='App'>{todoItems}</div>
   }
 }
 
--- react-workspace/todo-react-app/src/Todo.js
+++ react-workspace/todo-react-app/src/Todo.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class Todo extends React.Component {
+    constructor(props){
+        super(props); // porps 오브젝트 초기화
+        this.state = {item: props.item}; // state는 리액트가 관리하는 오브젝트
+    }
     render(){
         return (
             <div className='Todo'>
-                <input type="checkbox" id="todo0" name="todo0" value="todo0"/>
-                <label for = "todo0">Todo 컴포넌트 만들기</label>
+                <input type="checkbox"
+                // 자바스크립트로 된 변수를 JSX에서 사용하기 위해 {}로 변수를 묶어줌
+                // HTML안에서도 자바스크립트 사용 가능
+                 id={this.state.item.id}
+                name={this.state.item.id} 
+                checked={this.state.item.done}/>
+                <label id={this.state.item.id}>{this.state.item.title}</label>
             </div>
         );
     }
