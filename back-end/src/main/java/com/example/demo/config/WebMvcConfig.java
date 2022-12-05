4632c468354b3789e7abeed666c1c161a1dde05f
nikeisme <nikeisme827@gmail.com>
2022-12-05 16:14:25 +0900

    Add : 백엔드와 프론트엔드 연결

--- back-end/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ back-end/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -11,7 +11,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+
+                // Origin이 http://localhost:3000에 대해
                 .allowedOrigins("http://localhost:3000")
+                // "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS" 메서드를 허용한다.
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -1,62 +1,67 @@
-import React from 'react';
-import Todo from './Todo';
+import React from "react";
+import Todo from "./Todo";
 import AddTodo from "./AddTodo.js";
 import { Paper, List, Container } from "@material-ui/core";
-import './App.css';
+import "./App.css";
+import { call } from "./service/ApiService";
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
-   
     this.state = {
-      items : 
-      [],
+      items: [],
     };
   }
 
-  //(1)함수 추가
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
 
   add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length // key를 위한 id 추가
-    item.done = false; // done 초기화
-    thisItems.push(item); //리스트에 아이템 추가
-    this.setState({ items:thisItems}); // 업데이트는 반드시 this.setState로 해야됨
-    console.log("items : ",this.state.items);
-  }
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
 
   delete = (item) => {
-    const thisItems = this.state.items;
-    console.log("Before update Items : ", this.state.items)
-    const newItems = thisItems.filter( e => e.id !== item.id);
-    this.setState({ items: newItems }, () => {
-      // 디버깅 콜백
-      console.log("Update Items : ", this.state.items)
-    });
-  }
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
 
   render() {
-   
-   var todoItems = this.state.items.length > 0 && (
-    <Paper style={{ margin: 16 }}>
-      <List>
-        {this.state.items.map((item, idx) => (
-          <Todo item = {item} key = {item.id}  delete={this.delete} />
-          
-        ))}
-      </List>
-    </Paper>
-   );
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo
+              item={item}
+              key={item.id}
+              delete={this.delete}
+              update={this.update}
+            />
+          ))}
+        </List>
+      </Paper>
+    );
 
-   // (2) 함수 연결
-   return (
-    <div className="App">
-      <Container maxWidth ="md">
-        <AddTodo add ={this.add} />
-        <div className="TodoList">{todoItems}</div>
-      </Container>
-    </div>
-   )
+    // 3. props로 넘겨주기
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
