80b4c5146cc2d57d642ddcd4089f5b757a13a27e
nikeisme <nikeisme827@gmail.com>
2022-12-05 13:38:15 +0900

    Add : 프론트엔드 폴더 생성 및 Todo 컴포넌트 생성

---
+++ react-workspace/todo-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Todo from './Todo';
+import './App.css';
+
+class App extends React.Component{
+  render() {
+    return (
+      <div className='App'>
+        <Todo />
+      </div>
+    );
+  }
+}
+
+export default App;
