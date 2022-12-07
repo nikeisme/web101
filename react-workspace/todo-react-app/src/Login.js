30ffcf6e1e0438aa2d32e1467fc1132a2c487d2b
nikeisme <nikeisme827@gmail.com>
2022-12-08 00:59:12 +0900

    Add : 로그인 컴포넌트

---
+++ react-workspace/todo-react-app/src/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Login extends React.Component {
+// eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+    render() {
+    return <p>로그인 페이지</p>;
+  }
+}
+
+export default Login;
